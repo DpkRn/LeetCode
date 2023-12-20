6ba9aeac1b243da84063afbac787638b5b590368
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2023-12-20 20:03:28 +0530

    Time: 23 ms (75.20%), Space: 89.6 MB (94.10%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,69 +1,29 @@
-class Solution
-{
-    public:
-        double findMedianSortedArrays(vector<int> &a, vector<int> &b)
-        {
-            int n1 = a.size();
-            int n2 = b.size();
-            int l = 0, r = 0;
-            int n = (n1 + n2);
-            int ind2 = n / 2;
-            int ind1 = ind2 - 1;
-            int ind1el = -1;
-            int ind2el = -1;
-            int cnt = 0;
-            int flag = false;
-            while (l < n1 && r < n2 && flag == false)
-            {
-                if (a[l] < b[r])
-                {
-                    if (cnt == ind1) ind1el = a[l];
-                    if (cnt == ind2)
-                    {
-                        ind2el = a[l];
-                        flag = true;
-                    }
-                    cnt++;
-                    l++;
-                }
-                else
-                {
-                    if (cnt == ind1) ind1el = b[r];
-                    if (cnt == ind2)
-                    {
-                        ind2el = b[r];
-                        flag = true;
-                    }
-                    cnt++;
-                    r++;
-                }
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1=nums1.size(),n2=nums2.size();
+        if(n1>n2) return findMedianSortedArrays(nums2,nums1);
+        int low=0; int high=n1;
+        int n=n1+n2;
+        int leftArr=n/2;
+        while(low<=high){
+            int mid1=(low+high)>>1;
+            int mid2=leftArr-mid1;
+            int l1=mid1-1<0?INT_MIN:nums1[mid1-1];
+            int l2=mid2-1<0?INT_MIN:nums2[mid2-1];
+            int r1=mid1<n1?nums1[mid1]:INT_MAX;
+            int r2=mid2<n2?nums2[mid2]:INT_MAX;
+            
+            if(l1<=r2&&l2<=r1){
+                if(n%2==0) return (double)(max(l1,l2)+min(r1,r2))/2.0;
+                else return min(r1,r2);
+            }else if(l1>r2){
+                high=mid1-1;
+            }else {
+                low=mid1+1;
             }
-            while (l < n1 && flag == false)
-            {
-                if (cnt == ind1) ind1el = a[l];
-                if (cnt == ind2)
-                {
-                    ind2el = a[l];
-                    flag = true;
-                }
-                cnt++;
-                l++;
-            }
-            while (r < n2 && flag == false)
-            {
-                if (cnt == ind1) ind1el = b[r];
-                if (cnt == ind2)
-                {
-                    ind2el = b[r];
-                    flag == true;
-                }
-                cnt++;
-                r++;
-            }
-
-            if (n % 2 == 1)
-                return (double)(ind2el);
-
-            return (double)(ind1el + ind2el) / 2.0;
+            
         }
+        return 0;
+    }
 };
