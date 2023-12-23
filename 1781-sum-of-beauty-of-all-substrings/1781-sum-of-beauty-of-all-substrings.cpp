49f8578986e776de26c9112e13678d4b98499c8c
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2023-12-24 03:56:32 +0530

    Time: 281 ms (46.19%), Space: 24 MB (47.92%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -23,29 +23,3 @@ public:
     }
 };
 
-// class Solution {
-// public:
-// int res(unordered_map<char,int>&mp){
-//     int mf=-1;
-//     int lf=INT_MAX;
-//     for(auto &it:mp){
-//         mf=max(mf,it.second);
-//         if(it.second>=1){
-//             lf=min(lf,it.second);
-//        }
-       
-//     }
-//      return mf-lf;
-// }
-//     int beautySum(string s) {
-//         int ans=0;
-//         for(int i=0;i<s.size();i++){
-//             unordered_map<char,int>mp;
-//             for(int j=i;j<s.size();j++){
-//                 mp[s[j]]++;
-//                 ans=ans+res(mp);
-//             }
-//         }
-//         return ans;
-//     }
-// };
