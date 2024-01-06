06c6ec3c77e586f300de9485e9361d398258f15b
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-01-07 03:55:30 +0530

    Time: 3 ms (42.09%), Space: 6.9 MB (43.62%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
-    void solve(int ind,string temp,string &digits,vector<string>&ans,map<char,string> mp){
+    void solve(int ind,string temp,string &digits,vector<string>&ans,vector<string> &mp){
     if(ind>=digits.size()){
         if(temp!="")
         ans.push_back(temp);
         return;
     }
-    for(int i=0;i<mp[digits[ind]].size();i++){
-        solve(ind+1,temp+mp[digits[ind]][i],digits,ans,mp);
+    for(int i=0;i<mp[digits[ind]-'0'].size();i++){
+        solve(ind+1,temp+mp[digits[ind]-'0'][i],digits,ans,mp);
     }
 }
     vector<string> letterCombinations(string digits) {
-         map<char,string> mp;
-   
-    mp['2']="abc";
-    mp['3']="def";
-    mp['4']="ghi";
-    mp['5']="jkl";
-    mp['6']="mno";
-    mp['7']="pqrs";
-    mp['8']="tuv";
-    mp['9']="wxyz";
+    vector<string> mp={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     vector<string> ans;
     solve(0,"",digits,ans,mp);
         return ans;
