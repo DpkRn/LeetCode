da5543ffb6752caab98f7dd843526e3d2e7e5c51
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-01-12 23:44:36 +0530

    Time: 3 ms (16.55%), Space: 6.5 MB (35.41%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,19 +1,110 @@
 class Solution {
- 
+public:
+    int f(int n,int k,vector<int>&dp){
+        if(n==0) return 1;
+        if(dp[n]!=-1) return dp[n];
+        int allWay=0;
+        for(int i=1;i<=k;i++){
+            if(n-i>=0)
+                allWay+=f(n-i,k,dp);
+        }
+        return dp[n]=allWay;
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return f(n,2,dp);
+    }
+};
+
+/*
+class Solution {
+public:
+    int climbStairs(int n) {
+        // vector<int> dp(n+1,0);
+       // dp[0]=1;
+        int prev1=1,prev2=0;
+        for(int i=1;i<=n;i++){
+            int prev1=prev2;
+            int prev2=0;
+            if(i-2>=0)
+               prev2=dp[i-2];
+            dp[i]=left+right;
+        }
+        
+        return dp[n];
+    }
+};
+/*
+
+
+/*
+O(N) O(N)
+class Solution {
 public:
     int climbStairs(int n) {
         vector<int> dp(n+1,0);
-       int a=1;
-        int b=1;
-        int c;
-       if(n==1||n==0) return 1;
-        for(int i=2;i<=n;i++){
-            c=a+b;
-            a=b;
-            b=c; 
-            
-         }
-    return c;
+        dp[0]=1;
+        for(int i=1;i<=n;i++){
+            int left=dp[i-1];
+            int right=0;
+            if(i-2>=0)
+               right=dp[i-2];
+            dp[i]=left+right;
+        }
+        
+        return dp[n];
     }
-    
 };
+*/
+
+
+/*
+O(N) O(1)
+class Solution {
+public:
+    int climbStairs(int n) {
+        int prev1=1,prev2=0;
+        for(int i=1;i<=n;i++){
+             int curr=prev1+prev2;
+            prev1=curr;
+            prev2=prev1;         
+        }
+        return prev1;
+        
+    }
+};
+*/
+
+
+
+
+/*
+
+O(N)  O((vector)N+(stack)N)
+
+class Solution {
+public:
+    int solve(int n,vector<int> &dp){
+        if(n==0) return 1; //from 0 to 0 one way
+        if(dp[n]!=-1) return dp[n];
+        int left=solve(n-1,dp);
+        int right=0;
+        if(n-2>=0)   //if its going beyond 0 that means this is unvalid move so dont count.
+            right=solve(n-2,dp);
+        return dp[n]=left+right;
+        
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);   
+        
+    }
+};
+
+*/
+
+ 
+       
+
+
+
