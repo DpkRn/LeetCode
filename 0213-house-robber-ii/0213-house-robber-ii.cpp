9990e3ee7395a9b4f57e482852cc4c1fa914054e
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-01-13 01:52:34 +0530

    Time: 2 ms (43.42%), Space: 8.6 MB (6.93%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,3 +1,36 @@
+
+class Solution {
+public:
+ 
+    int solve(vector<int>& nums,int n,vector<int> &dp){
+         if(n==0)
+            return nums[n];
+        if(n<0)
+            return 0;
+        if(dp[n]!=-1) return dp[n];   
+        int pick=nums[n]+solve(nums,n-2,dp);
+        int notPick=0+solve(nums,n-1,dp);
+        return dp[n]=max(pick,notPick);
+    }
+    
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if (n==1) return nums[0];
+        vector<int> temp1,temp2;
+        for(int i=0;i<n;i++){
+            if(i!=0) temp1.push_back(nums[i]);
+            if(i!=n-1) temp2.push_back(nums[i]);
+        }
+          vector<int> dp(n-1,-1);
+        int left=solve(temp1,n-2,dp);
+        for(int i=0;i<n-1;i++) dp[i]=-1;
+        int right=solve(temp2,n-2,dp);
+        return max(left,right);
+        
+    }
+};
+
+/*
 class Solution {
 public:
     int f(int n,int initial,vector<int>&nums,vector<vector<int>>&dp){
@@ -12,8 +45,7 @@ public:
         int n=nums.size();
         if(n==1) return nums[0];   
         vector<vector<int>>dp(n,vector<int>(2,-1));
-        return max(f(n-1,1,nums,dp),f(n-2,0,nums,dp));
-        
-        
+        return max(f(n-1,1,nums,dp),f(n-2,0,nums,dp));    
     }
 };
+*/
