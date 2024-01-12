ff90db9d540d3c7f2d77a9fb814a593a5c7a5ee2
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-01-13 01:16:41 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (9.63%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,5 +1,26 @@
 class Solution {
 public:
+    int rob(vector<int>& nums) {
+      int n=nums.size();
+      vector<int> dp(n,0);
+        if(n==1) return nums[0];
+        if(n==2) return max(nums[1],nums[0]);
+        dp[0]=nums[0];
+        
+        dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+           int notpick=dp[i-1];
+           //int pick=nums[i];
+              int  pick=nums[i]+dp[i-2];
+            dp[i]=max(pick,notpick);
+        }
+        return dp[n-1];
+    }
+};
+
+/*
+class Solution {
+public:
     int f(vector<int>&nums,int n,vector<int>&dp){
         if(n==0) return nums[n];
         if(dp[n]!=-1) return dp[n];
@@ -17,3 +38,4 @@ public:
         
     }
 };
+*/
