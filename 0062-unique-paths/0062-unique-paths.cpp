01df940d00d7cbcfca6b1e594dafb10877892f96
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-01-13 16:20:36 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (71.95%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
+        vector<int> dp(n,0);
+        int rt=0;
+        for(int r=m-1;r>=0;r--){    
+            for(int c=n-1;c>=0;c--){
+                if(r==m-1&&c==n-1) {dp[n-1]=1;rt=1; continue;}
+                int bot=0;
+                int right=0;
+                if(c+1<=n-1)
+                right=rt;
+                if(r+1<=m-1)
+                bot=dp[c];
+                dp[c]=bot+right;
+                rt=dp[c];                
+            }           
+        }
+        return dp[0];
+    }
+};
+
+/*class Solution {
+public:
+    int uniquePaths(int m, int n) {
          vector<vector<int>>dp(m,vector<int>(n,0));
         dp[m-1][n-1]=1;
         for(int r=m-1;r>=0;r--){    
@@ -19,6 +41,7 @@ public:
         return dp[0][0];
     }
 };
+*/
 
 /*
 class Solution {
