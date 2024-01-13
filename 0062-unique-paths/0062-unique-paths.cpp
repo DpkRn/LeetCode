6b4e1e683e75cdf925273e1f0e159803921d8d2d
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-01-13 15:04:37 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (20.93%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,17 +1,16 @@
-int fun(int i, int j, int m, int n, vector<vector<int>>& dp){
-    
-    if(i>=m or j>=n) return 0;
-    if(i==m-1 and j==n-1) return 1;
-    if(dp[i][j]!=-1) return dp[i][j];
-    int bot=fun(i+1,j,m,n,dp);
-    int right=fun(i,j+1,m,n,dp);
-    return dp[i][j]=bot+right;
-    
-}
 class Solution {
+    int f(int r,int c,int m,int n,vector<vector<int>>&dp){
+        if(r>=m||c>=n) return 0;
+        if(dp[r][c]!=-1) return dp[r][c];
+        if(r==m-1&&c==n-1) return 1;
+        int bot=f(r+1,c,m,n,dp);
+        int right=f(r,c+1,m,n,dp);
+        
+        return dp[r][c]=bot+right;
+    }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n,-1));
-        return fun(0,0,m,n,dp);
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return f(0,0,m,n,dp);
     }
 };
