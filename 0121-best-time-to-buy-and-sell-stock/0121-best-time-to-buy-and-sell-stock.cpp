d0b29ed12094991a654bf8191c302141c18cfeb3
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-01-21 17:24:12 +0530

    Time: 96 ms (62.58%), Space: 95.6 MB (6.07%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,3 +1,6 @@
+
+
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
@@ -14,3 +17,20 @@ public:
         return maxi;       
     }
 };
+
+
+
+/*
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxi=0;
+        int mini=prices[0];
+        for(int i=1;i<prices.size();i++){
+            mini=min(prices[i],mini);
+            maxi=max(maxi,prices[i]-mini);
+        }
+        return maxi;
+    }
+};
+*/
