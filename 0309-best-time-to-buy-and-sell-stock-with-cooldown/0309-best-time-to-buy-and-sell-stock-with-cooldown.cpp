20e5f521a0cf30e0192ef996e3f51a70d39832bb
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-01-22 18:15:09 +0530

    Time: 4 ms (57.62%), Space: 13.9 MB (8.01%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    int f(int ind,int buy,auto &prices,auto &dp){
-        if(ind>=prices.size()) return 0;
-        if(dp[ind][buy]!=-1) return dp[ind][buy];
-        if(buy)
-            return dp[ind][buy]= max(-prices[ind]+f(ind+1,!buy,prices,dp),f(ind+1,buy,prices,dp));
-        return dp[ind][buy]= max(prices[ind]+f(ind+2,!buy,prices,dp),f(ind+1,buy,prices,dp));
-        
-    }
     int maxProfit(vector<int>& prices) {
-        int buy=true;
         int n=prices.size();
-        vector<vector<int>>dp(n,vector<int>(2,-1));
-        return f(0,buy,prices,dp);
+        vector<vector<int>> dp(n+2,vector<int> (2,0));
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<2;buy++){
+                if(buy)
+                    dp[ind][buy]=max(-prices[ind]+dp[ind+1][!buy],dp[ind+1][buy]);
+                else
+                    dp[ind][buy]=max(prices[ind]+dp[ind+2][!buy],dp[ind+1][buy]);                    
+            }
+        }
+        return dp[0][1];
     }
 };
