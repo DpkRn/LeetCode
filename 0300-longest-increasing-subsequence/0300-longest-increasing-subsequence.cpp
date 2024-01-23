469cb6608ec3f9f499f1445cc17f3c48f24978ff
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-01-24 00:28:35 +0530

    Time: 10 ms (74.01%), Space: 12.9 MB (22.99%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,19 +1,61 @@
+class Solution
+{
+    public:
+        int lengthOfLIS(vector<int> &nums)
+        {
+            vector<int> v;
+            int n = nums.size();
+            v.push_back(nums[0]);
+            for (int i = 0; i < n; i++)
+            {
+                if (nums[i] > v.back()) v.push_back(nums[i]);
+                else
+                {
+                    int n = lower_bound(v.begin(), v.end(), nums[i]) - v.begin();
+                    v[n] = nums[i];
+                }
+            }
+            return v.size();
+        }
+};
+/*
 class Solution {
 public:
-    // int f(int ind,int prev,auto &nums,auto &dp){
-    //     if(ind==nums.size()) return 0;
-    //     if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
-    //     int pick=INT_MIN;
-    //     if(prev==-1||nums[prev]<nums[ind])
-    //         pick=1+f(ind+1,ind,nums,dp);
-    //         pick=max(pick,f(ind+1,prev,nums,dp));        
-    //     return dp[ind][prev+1]=pick;     
-    // }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        // vector<vector<int>>dp(n,vector<int> (n+1,-1));
-        // return f(0,-1,nums,dp);
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        vector<int> dp(n,1);
+        for(int curr=0;curr < n;curr++){
+            for(int prev=0;prev < curr;prev++){
+                if(nums[prev] < nums[curr])
+                    dp[curr]=max(1+dp[prev],dp[curr]);
+            }
+        }
+        int ans=0;
+        for(int i=0;i < n;i++) {
+            ans=max(ans,dp[i]);
+        }
+        return ans;
+    }
+};
+
+*/
+/*
+class Solution {
+public:
+   	// int f(int ind,int prev,auto &nums,auto &dp){
+   	//     if(ind==nums.size()) return 0;
+   	//     if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
+   	//     int pick=INT_MIN;
+   	//     if(prev==-1||nums[prev] < nums[ind])
+   	//         pick=1+f(ind+1,ind,nums,dp);
+   	//         pick=max(pick,f(ind+1,prev,nums,dp));        
+   	//     return dp[ind][prev+1]=pick;     
+   	// }
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+       	// vector<vector<int>>dp(n,vector<int> (n+1,-1));
+       	// return f(0,-1,nums,dp);
+        vector<vector < int>> dp(n+1,vector<int>(n+1,0));
         for(int ind=n-1;ind>=0;ind--){
             for(int prev=n-1;prev>=-1;prev--){
                dp[ind][prev+1]=dp[ind+1][prev+1];
@@ -24,3 +66,4 @@ public:
         return dp[0][0];
     }
 };
+*/
