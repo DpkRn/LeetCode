493d3cee9a284b86d962207fbaa188ae4875952d
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-01-28 13:08:55 +0530

    Time: 452 ms (62.26%), Space: 7.8 MB (53.69%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,47 +1,28 @@
-class Solution
-{
-    public:
-        bool isPalindrome ( int i, int j,string & s){
+class Solution {
+public:  
+    bool isPalindrome ( int i, int j,string & s){
         while(i<j){
             if(s[i]!=s[j]) return false;
             i++; j--;
         }
         return true;
     }
-    int f(int i, string &s, auto &dp)
-    {
-         if(i>=s.size() or isPalindrome(i,s.size(),s)) return 0;
-        if (dp[i] != -1) return dp[i];
-        int mini = INT_MAX;
-        for (int j = i; j < s.size(); j++)
-        {
-            if (isPalindrome(i, j, s))
-            {
-                int cnt = 1 + f(j + 1, s, dp);
-                mini = min(mini, cnt);
+    int f(int n,string &s,auto &dp){
+        if(n==0||isPalindrome(0,n,s)) return 0;
+        if(dp[n]!=-1) return dp[n];
+        int mini=INT_MAX;
+        for(int i=0;i<=n;i++){
+            if(isPalindrome(i,n,s)){
+                int cnt=1+f(i-1,s,dp);
+                mini=min(cnt,mini);
             }
         }
-        return dp[i] = mini;
+        return dp[n]=mini;
     }
-    int minCut(string s)
-    {
-        int n = s.size();
-        vector<int> dp(n, -1);
-        return f(0, s, dp) - 1;
-        // vector<int> dp(n + 1, 0);
-        // for (int i = n-1; i >= 0; i--)
-        // {
-        //     int mini = INT_MAX;
-        //     for (int j = i; j < s.size(); j++)
-        //     {
-        //         if (isPal(i, j, s))
-        //         {
-        //             int cnt = 1 + dp[j + 1];
-        //             mini = min(mini, cnt);
-        //         }
-        //         dp[i] = mini;
-        //     }
-        // }
-        // return dp[0]-1;
+    int minCut(string s) {
+        int n=s.size();
+        vector<int> dp(n+1,-1);
+        return f(n-1,s,dp);
+        
     }
 };
