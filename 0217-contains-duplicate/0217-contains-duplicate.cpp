df6299e5820c896768741477b45ed048a868edce
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-02-27 13:43:08 +0530

    Time: 130 ms (23.32%), Space: 73.2 MB (21.26%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int,bool > mp;
+        for(int i=0;i<nums.size();i++){
+            if(mp[nums[i]]==true) return true;
+            mp[nums[i]]=true;
+        }
+        return false;
+    }
+};
