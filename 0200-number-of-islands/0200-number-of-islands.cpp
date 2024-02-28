30485e64fdb1938229e756318fc5b2e657d432d9
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-02-28 19:24:51 +0530

    Time: 22 ms (94.84%), Space: 22.7 MB (21.73%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,3 +1,48 @@
+//using BFS
+class Solution {
+public:
+    int dx[4]={0,0,1,-1};
+    int dy[4]={1,-1,0,0};
+    bool isValid(int r,int c,vector<vector<char>>& grid){
+        int m=grid.size();
+        int n=grid[0].size();
+        if(r>=m||r<0||c>=n||c<0||grid[r][c]=='0') return false;
+        return true;
+    }
+    void bfs(int r,int c,vector<vector<char>> &grid,int m,int n){
+        queue<pair<int,int>> q;
+        q.push({r,c});
+        grid[r][c]='0';
+        while(!q.empty()){
+            int rc=q.front().first;
+            int cc=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                if(isValid(rc+dx[i],cc+dy[i],grid)){
+                    q.push({rc+dx[i],cc+dy[i]});
+                    grid[rc+dx[i]][cc+dy[i]]='0';
+                }
+            }
+        }
+        
+    }
+    int numIslands(vector<vector<char>>& grid) {
+         int m=grid.size();
+        int n=grid[0].size();
+        int cnt=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='1'){
+                    bfs(i,j,grid,m,n);
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+};
+/*
+//Using DFS
 class Solution {
 public:
     vector<int> dx={0,0,1,-1};
@@ -25,3 +70,4 @@ public:
         return cnt;
     }
 };
+*/
