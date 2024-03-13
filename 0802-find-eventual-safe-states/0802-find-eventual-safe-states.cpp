e924e4c10879407f3e2e21130098c640562ac1a2
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-03-13 20:42:45 +0530

    Time: 111 ms (80.12%), Space: 50.2 MB (95.34%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,29 +1,51 @@
 class Solution {
 public:
-    bool dfs(int node,vector<vector<int>> &adj,vector<int> &vis,vector<int> &path){
-        vis[node]=1;
-        path[node]=1;
-        for(auto adjNode:adj[node]){
-            //node has not been visited then
-            if(!vis[adjNode]){
-                if(dfs(adjNode,adj,vis,path)) return true;
-            }else 
-            //node has already in path then
-            if(path[adjNode]==1) return true;
+    bool dfs(int src,vector<vector<int>>&graph,int vis[],int path[],int isState[]){
+        vis[src]=1;
+        path[src]=1;
+        isState[src]=0;
+        for(auto it:graph[src]){
+            if(!vis[it]){
+                if(dfs(it,graph,vis,path,isState)) 
+                {
+                    isState[src]=0;
+                   // path[src]=0;
+                    return true; 
+                }
+            }else{
+                if(path[it]){
+                    isState[src]=0;
+                   // path[src]=0;
+                    return true; ;
+                }
+            }
         }
-        path[node]=0;
+        path[src]=0;
+        isState[src]=1;
         return false;
     }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        vector<int> states;
         int n=graph.size();
+
+        int vis[n];
+        int path[n];
+        int isState[n];
         for(int i=0;i<n;i++){
-            vector<int> vis(n,0);
-            vector<int> path(n,0);
-            if(dfs(i,graph,vis,path)) 
-            continue;
-            states.push_back(i);
+                vis[i]=0;
+            path[i]=0;
+            isState[i]=0;
         }
-        return states;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(i,graph,vis,path,isState);
+            }
+        }
+     vector<int> vec;
+        for(int i=0;i<n;i++){
+            if(isState[i]){
+                vec.push_back(i);
+            }
+        }
+        return vec;
     }
 };
