0c2d601e9d1ebe4531bfd9e7e209891493cd1833
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-03-14 00:58:46 +0530

    Time: 145 ms (44.14%), Space: 62 MB (56.27%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,6 +1,38 @@
 class Solution {
 public:
-    bool dfs(int src,vector<vector<int>>&graph,int vis[],int path[],int isState[]){
+    //using bfs(kahn's algorithm or topoSort)
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<int> adj[n];
+        int indegree[n];
+        memset(indegree,0,sizeof(indegree));
+        for(auto i=0;i<n;i++){
+            for(auto it:graph[i]){
+                indegree[i]++;
+                adj[it].push_back(i);
+            }
+        }
+        queue<int> q;
+       for(int i=0;i<n;i++){
+           if(indegree[i]==0) q.push(i);
+       }
+        vector<int> ans;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
+//USING DFS
+/*
+bool dfs(int src,vector<vector<int>>&graph,int vis[],int path[],int isState[]){
         vis[src]=1;
         path[src]=1;
         isState[src]=0;
@@ -48,4 +80,4 @@ public:
         }
         return vec;
     }
-};
+*/
