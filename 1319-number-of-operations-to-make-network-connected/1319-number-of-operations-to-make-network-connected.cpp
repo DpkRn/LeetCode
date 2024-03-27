19747b3d5f7fe4109af43529dd1ca98bb9869be1
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-03-27 21:50:16 +0530

    Time: 95 ms (42.46%), Space: 45.8 MB (67.90%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,7 +1,8 @@
 class disjointSet{
+    public:
     vector<int> size;
     vector<int> parent;
-    public:
+    
     disjointSet(int n){
         size.resize(n,1);
         parent.resize(n);
@@ -24,10 +25,7 @@ class disjointSet{
             size[epu]+=size[epv];
         }
     }
-    vector<int> getParent(){
-        return parent;
-    }
-      
+   
 };
 class Solution {
 public:
@@ -38,15 +36,11 @@ public:
             if(dsu.findUPar(it[0])==dsu.findUPar(it[1])) cnt++;
             else dsu.unionBySize(it[0],it[1]);
         }
-        for(int i=0;i<n;i++) dsu.findUPar(i);
-        cout<<cnt<<endl;
-        set<int> st;
-        vector<int> parent=dsu.getParent();
-        for(auto it:parent) cout<<it<<" ";
-        for(auto it:parent)
-            st.insert(it);
-        int remaining=st.size()-1;
-        if(remaining<=cnt) return remaining;
+        
+        int noOfComponents=0;
+        for(int i=0;i<n;i++) if(dsu.parent[i]==i) noOfComponents++;
+         int reqEdge=noOfComponents-1;
+        if(reqEdge<=cnt) return reqEdge;
         else return -1;
     }
 };
