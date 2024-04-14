ac2e2c870c166de507a27e4cf3aa3b9de973787f
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-04-15 04:17:43 +0530

    Time: 3 ms (99.05%), Space: 22.4 MB (52.06%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,31 +1,16 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-       
         int n=height.size();
-        
-        int left[n];int right[n];
-        left[0]=height[0];
-         right[n-1]=height[n-1];
-        //finding leftgreatest and right greatest of each index
-        for(int i=1;i<n;i++){
-            left[i]=max(left[i-1],height[i]);
-            right[n-i-1]=max(right[n-i],height[n-1-i]);
-        }
-      
-        for(auto x:left){
-            cout<<x<<" ";
-        }
-        cout<<endl;
-        for(auto x:right){
-            cout<<x<<" ";
-        }
-        cout<<endl;
-        int sum=0;
+        int left[n],right[n];
         for(int i=0;i<n;i++){
-            sum+=min(left[i],right[i])-height[i];
+            left[i]=i-1>=0?max(left[i-1],height[i]):height[0];
+            right[n-1-i]=n-i<n?max(right[n-i],height[n-1-i]):height[n-1];
         }
-       
-        return sum;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans+=min(left[i],right[i])-height[i];
+        }
+        return ans;
     }
 };
