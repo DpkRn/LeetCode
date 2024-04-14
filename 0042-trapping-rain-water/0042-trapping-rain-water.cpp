2d3468b9abb4b4c0cead5cb3414ca383a4a70dfa
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-04-14 13:44:37 +0530

    Time: 47 ms (5.03%), Space: 22.6 MB (45.46%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        stack<int> st;
+       
         int n=height.size();
-        st.push(height[0]);
-        int left[n];
-        left[0]=height[0];
-        for(int i=1;i<n;i++){
-            if(st.top()>=height[i]){
-                left[i]=st.top();
-            }
-            else{
-                st.push(height[i]);
-                left[i]=height[i];
-            }
-        }
-        while(!st.empty()) st.pop();
         
-        st.push(height[n-1]);
-        int right[n];
-        right[n-1]=height[n-1];
-        for(int i=n-2;i>=0;i--){
-            if(st.top()>height[i]){
-                right[i]=st.top();
-            }
-            else{
-                st.push(height[i]);
-                right[i]=height[i];
-            }
+        int left[n];int right[n];
+        left[0]=height[0];
+         right[n-1]=height[n-1];
+        //finding leftgreatest and right greatest of each index
+        for(int i=1;i<n;i++){
+            left[i]=max(left[i-1],height[i]);
+            right[n-i-1]=max(right[n-i],height[n-1-i]);
         }
+      
+        for(auto x:left){
+            cout<<x<<" ";
+        }
+        cout<<endl;
+        for(auto x:right){
+            cout<<x<<" ";
+        }
+        cout<<endl;
         int sum=0;
         for(int i=0;i<n;i++){
             sum+=min(left[i],right[i])-height[i];
         }
-        // for(auto it:left) cout<<it<<" "; cout<<endl;
-        // for(auto it:right) cout<<it<<" ";
+       
         return sum;
     }
 };
