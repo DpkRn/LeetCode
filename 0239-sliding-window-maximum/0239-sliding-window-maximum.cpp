82851cae8a118d02d4905b73f08032f16d4eab29
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-04-16 21:47:12 +0530

    Time: 175 ms (57.15%), Space: 136.9 MB (93.99%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,3 +1,58 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {        
+       deque<int> dq;
+        vector<int> ans;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(!dq.empty()&&i-k==dq.front()) dq.pop_front();
+            
+            while(!dq.empty()&&nums[dq.back()]<nums[i]) dq.pop_back();
+            
+            dq.push_back(i);
+            if(i>=k-1) ans.push_back(nums[dq.front()]);
+        }
+       
+        return ans;
+    }
+};
+    
+
+/*
+//using sliding windows
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n=nums.size();
+        map<int,int> cnt;
+        int maxi=INT_MIN;
+        vector<int> ans(n-k+1);
+        int j=0;
+        for( j=0;j<k;j++){
+            maxi=max(maxi,nums[j]);
+            cnt[nums[j]]++;
+        }       
+        ans[0]=maxi;
+        int i=1;
+        while(j<n){            
+            if(maxi==nums[i-1]&&cnt[nums[i-1]]==1){
+                maxi=*max_element(nums.begin()+i,nums.begin()+j+1); 
+            }
+            else{
+                maxi=max(maxi,nums[j]);
+            }
+             cnt[nums[i-1]]--;
+            cnt[nums[j]]++;
+            
+            ans[i]=maxi;
+            i++; j++;
+        }
+        return ans;
+    }
+};
+*/
+/*
+//USING SIGMENT TREE
 #define N (int)1e5+2
 class Solution {
 public:
@@ -34,3 +89,4 @@ public:
         return ans;
     }
 };
+*/
