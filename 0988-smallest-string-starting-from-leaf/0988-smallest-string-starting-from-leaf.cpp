52e4568ee87ff9bd2073540f5ad3ce771472f435
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-04-18 01:25:51 +0530

    Time: 13 ms (21.75%), Space: 20.4 MB (66.14%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.cpp
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.cpp
@@ -11,6 +11,25 @@
  */
 class Solution {
 public:
+    string fun(TreeNode* root,string curr){
+        if(root==NULL) return "";
+        curr=char(root->val+'a')+curr;
+        if(root->left&&root->right){
+            string s1=fun(root->left,curr);
+            string s2=fun(root->right,curr);
+            return s1<s2?s1:s2;
+        }
+        if(root->left) return fun(root->left,curr);
+        if(root->right) return fun(root->right,curr);
+        return curr;
+    }
+    string smallestFromLeaf(TreeNode* root) {
+        return fun(root,"");
+    }
+};
+/*
+class Solution {
+public:
     void solve(TreeNode* root,string path,string &str){
         if(root==NULL) return ;
         path=(char)('a'+root->val)+path;
@@ -28,3 +47,4 @@ public:
         return str;
     }
 };
+*/
