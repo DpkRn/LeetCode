fa3b03636b203168374d7718bd6c3273d61be90d
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-04-26 03:39:52 +0530

    Time: 90 ms (8.88%), Space: 70.1 MB (66.79%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        int best=-1e9,sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum=max(nums[i],nums[i]+sum);
+            best=max(sum,best);
+        }
+        return best;
+    }
+};
+/*
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
         int sum=0,maxi=INT_MIN;
         for(int i=0;i<nums.size();i++){
             if(sum<0) sum=0;
@@ -10,3 +22,4 @@ public:
         return maxi;
     }
 };
+*/
