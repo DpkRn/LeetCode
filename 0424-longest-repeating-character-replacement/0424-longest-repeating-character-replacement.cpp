651fb3a193ba689602e6811b098aa6e73cf72592
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-04-26 18:45:11 +0530

    Time: 9 ms (53.60%), Space: 8.2 MB (60.03%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,3 +1,27 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+            int n = s.size();
+            int maxLen = 0,l=0,maxi=0;          
+            int hash[26]={0};
+        for (int i = 0; i < n; i++){
+              hash[s[i]-'A']++;
+              maxi=max(maxi,hash[s[i]-'A']);
+              while((i-l+1-maxi)>k){
+                  hash[s[l]-'A']--;
+                  maxi=0;
+                  for(int j=0;j<26;j++){
+                      maxi=max(maxi,hash[j]);
+                  }
+                  l++;
+              }
+            if(i-l+1-maxi<=k)
+             maxLen=max(maxLen,i-l+1);   
+        }
+    return maxLen;
+    }
+};
+/*
 class Solution
 {
     public:
@@ -23,3 +47,4 @@ class Solution
             return maxLen;
         }
 };
+*/
