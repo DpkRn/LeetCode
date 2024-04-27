c448dd62ed94d96fafe8f94ba0b4389a67f4ccc1
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-04-28 02:21:16 +0530

    Time: 154 ms (24.55%), Space: 14.8 MB (57.21%) - LeetHub

--- 0514-freedom-trail/0514-freedom-trail.cpp
+++ 0514-freedom-trail/0514-freedom-trail.cpp
@@ -1,26 +1,42 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int solve(int ind,int ptr,string ring,string key){
-        int n=ring.size();
-        //base case
-        if(ind>=key.size()) return 0;
-        if(dp[ind][ptr]!=-1) return dp[ind][ptr];
-        int steps=1e9;
-        for(int i=0;i<n;i++){
-            if(ring[i]==key[ind]){
-                steps=min(steps,min(abs(ptr-i),n-abs(ptr-i))+1+solve(ind+1,i,ring,key));
-            }
-        }
-     return dp[ind][ptr]=steps;
-    }
+    // vector<vector<int>> dp;
+    // int solve(int ind,int ptr,string ring,string key){
+    //     int n=ring.size();
+    //     //base case
+    //     if(ind>=key.size()) return 0;
+    //     if(dp[ind][ptr]!=-1) return dp[ind][ptr];
+    //     int steps=1e9;
+    //     for(int i=0;i<n;i++){
+    //         if(ring[i]==key[ind]){
+    //             steps=min(steps,min(abs(ptr-i),n-abs(ptr-i))+1+solve(ind+1,i,ring,key));
+    //         }
+    //     }
+    //  return dp[ind][ptr]=steps;
+    // }
     int findRotateSteps(string ring, string key) {
         int ptr=0;//ring ptr
         int ind=0;//key ptr
         int n=ring.size();
         int m=key.size();
-        dp.assign(m,vector<int>(n,-1));
-        return solve(ind,ptr,ring,key);
+        // dp.assign(m,vector<int>(n,-1));
+        // return solve(ind,ptr,ring,key);
+        
+        vector<vector<int>> dp(m+1,vector<int>(n,0));
+        for(int ind=m-1;ind>=0;ind--){
+            for(int ptr=n-1;ptr>=0;ptr--){
+                int steps=1e9;
+                for(int i=0;i<n;i++){
+                    if(ring[i]==key[ind]){
+                        steps=min(steps,min(abs(ptr-i),n-abs(ptr-i))+1+dp[ind+1][i]);
+                     } 
+                }
+                dp[ind][ptr]=steps;
+                
+            }
+        }
+        return dp[0][0];
+        
     }
 };
 /*
