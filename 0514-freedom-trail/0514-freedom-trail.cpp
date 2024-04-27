d9b9d56731f9ea733b66f1db006bb1958c9b11d1
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-04-28 02:12:39 +0530

    Time: 129 ms (29.96%), Space: 78.9 MB (16.22%) - LeetHub

--- 0514-freedom-trail/0514-freedom-trail.cpp
+++ 0514-freedom-trail/0514-freedom-trail.cpp
@@ -1,5 +1,31 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
+    int solve(int ind,int ptr,string ring,string key){
+        int n=ring.size();
+        //base case
+        if(ind>=key.size()) return 0;
+        if(dp[ind][ptr]!=-1) return dp[ind][ptr];
+        int steps=1e9;
+        for(int i=0;i<n;i++){
+            if(ring[i]==key[ind]){
+                steps=min(steps,min(abs(ptr-i),n-abs(ptr-i))+1+solve(ind+1,i,ring,key));
+            }
+        }
+     return dp[ind][ptr]=steps;
+    }
+    int findRotateSteps(string ring, string key) {
+        int ptr=0;//ring ptr
+        int ind=0;//key ptr
+        int n=ring.size();
+        int m=key.size();
+        dp.assign(m,vector<int>(n,-1));
+        return solve(ind,ptr,ring,key);
+    }
+};
+/*
+class Solution {
+public:
     int fun(int k,int r,string key,string ring,auto &dp){
        if(k>=key.size()) return 0;
         if(dp[k][r]!=-1) return dp[k][r];
@@ -21,8 +47,7 @@ public:
             }
             anti++;
         }
-        return dp[k][r]=min(anti,clock);
-        
+        return dp[k][r]=min(anti,clock);     
     }
     int findRotateSteps(string ring, string key) {
         int n=ring.size();
@@ -32,3 +57,4 @@ public:
         return fun(0,0,key,ring,dp);
     }
 };
+*/
