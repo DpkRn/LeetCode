2f48561a47aa11c3190153d7e6a751a4ac6b3c1f
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-05-01 15:01:54 +0530

    Time: 3 ms (42.21%), Space: 7.5 MB (26.16%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    
+    string reversePrefix(string word, char ch) {
+        int n=word.size();
+        
+        for(int i=0;i<n;i++){
+            if(ch==word[i]){
+                reverse(word.begin(),word.begin()+i+1);
+                break;
+            }
+            
+        }
+        return word;
+    }
+};
