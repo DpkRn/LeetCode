a772ed5a6b33e73be1d2b409ec202d5672c680b9
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-05-02 15:59:09 +0530

    Time: 94 ms (67.00%), Space: 69.7 MB (99.41%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -3,18 +3,15 @@ public:
     //to get strictely equal to we will find first <=k and after that we will substact <=k-1
     int get(auto &nums,int k){
         int n=nums.size();
-        int l=0,r=0,cnt=0,odd=0;
-        while(r<n){
-            if(nums[r]&1!=0) odd++;
+        int l=0,cnt=0,odd=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]%2==1) odd++;
             while(odd>k){
-                if(nums[l]&1!=0) odd--; l++;
+                if(nums[l]%2==1) odd--;l++;
             }
-            if(odd<=k){
-                cnt+=(r-l+1);
-            }
-            r++;           
+            if(odd<=k) cnt+=(i-l+1);
         }
-         return cnt;
+        return cnt;
     }
     int numberOfSubarrays(vector<int>& nums, int k) {
         return get(nums,k)-get(nums,k-1);
