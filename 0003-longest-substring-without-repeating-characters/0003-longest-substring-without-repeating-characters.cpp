41306feb5f0031ab7d7ac57d2f3bb7d94fc42a3f
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-05-02 15:38:27 +0530

    Time: 15 ms (55.55%), Space: 11 MB (70.72%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,18 +5,11 @@ public:
         int maxi=0,l=0;
         unordered_map<char,int> mp;
         for(int i=0;i<n;i++){
-            //expand till we get repeating
-            if(mp[s[i]]==0){
                 mp[s[i]]++;
-            }else{
-                //shrink till we get removed repeating character
-                while(mp[s[i]]>0){
+                while(mp[s[i]]>1){
                    mp[s[l]]--; 
                     l++;
                 }
-                //before moving next increase the count of current char
-                mp[s[i]]++;
-            }
             maxi=max(maxi,i-l+1);            
         }
         return maxi;
