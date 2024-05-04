9598e7b2cc48959b5a284251aa12453128afbecf
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-05-04 16:09:11 +0530

    Time: 67 ms (11.33%), Space: 51.9 MB (9.28%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,5 +1,28 @@
 class Solution {
 public:
+    int f(int ind,int a,auto &coins,auto &dp){
+        if(ind<0){
+            if(a==0) return 0; else return 1e9;
+        }
+        if(dp[ind][a]!=-1) return dp[ind][a];
+        int notpick=f(ind-1,a,coins,dp);
+        int pick=1e9;
+        if(a>=coins[ind])
+            pick=1+f(ind,a-coins[ind],coins,dp);
+        return dp[ind][a]=min(pick,notpick);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
+        int ans= f(n-1,amount,coins,dp);
+        if(ans==1e9) return -1;
+        else return ans;
+        
+    }
+};
+/*
+class Solution {
+public:
     int coinChange(vector<int>& coins, int amount) {
          int n=coins.size();
        vector<int> dp(amount+1,1e9);
@@ -7,13 +30,16 @@ public:
         for(int i=1;i<=n;i++){
             for(int j=0;j<=amount;j++){
                 
-                 if(j-coins[i-1]>=0) dp[j]=min(dp[j],1+dp[j-coins[i-1]]);
+                 if(j-coins[i-1]>=0) dp[j]=min(dp[j],1+dp[j-coins[i-1]]); 
             }
         }        
        if(dp[amount]==1e9) return -1;
        return dp[amount];
     }
 };
+
+*/
+
 /*
 class Solution {
 public:
@@ -31,4 +57,6 @@ public:
        return dp[n][amount];
     }
 };
+
 */
+
