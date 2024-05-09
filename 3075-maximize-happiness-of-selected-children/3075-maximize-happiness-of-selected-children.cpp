291d643617d674ce975344d05b865dfcfcf77abe
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-05-09 12:32:22 +0530

    Time: 204 ms (23.13%), Space: 116.7 MB (7.79%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        priority_queue<int> pq;
+        int n=happiness.size();
+        for(auto it:happiness){
+            pq.push(it);
+        }
+        long long decr=0;
+        long long  h=0;
+        while(k--){
+            int top=pq.top();
+            pq.pop();
+            if(top-decr>=0)
+            h+=(long long)(top-decr);
+            decr++;
+            
+        }
+        
+        return h;
+    }
+};
