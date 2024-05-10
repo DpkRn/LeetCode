268fb11d2163a95693478a0706131f944468add9
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-05-10 17:32:44 +0530

    Time: 20 ms (83.97%), Space: 27.3 MB (7.21%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,5 +1,35 @@
 class Solution {
 public:
+    int getSmallerThanM(auto &mat,int m){
+        int n=mat.size();
+        int cnt=0;
+        for(auto it:mat){
+            cnt+=upper_bound(it.begin(),it.end(),m)-it.begin();
+        }
+        return cnt;
+    }
+    int kthSmallest(vector<vector<int>>& mat, int k) {
+        int n=mat.size();
+        
+        int l=mat[0][0]; int h=mat[n-1][n-1];
+         int ans=-1;
+        while(l<=h){
+            int m=l+(h-l)/2;
+            int cnt=getSmallerThanM(mat,m);
+            if(cnt>=k){
+                ans=m;
+                h=m-1;
+            }else{
+                l=m+1;
+            }
+        }
+        return ans;
+    }
+};
+
+/*
+class Solution {
+public:
     //O(mnlog mn) O(mn)
     int kthSmallest(vector<vector<int>>& matrix, int k) {
         vector<int> ans;
@@ -10,3 +40,4 @@ public:
         return ans[k-1];
     }
 };
+*/
