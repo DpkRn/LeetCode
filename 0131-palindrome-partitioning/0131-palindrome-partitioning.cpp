e5b1cd1bc7f4b1a8b35c23999415fc6a03d9d8dc
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-05-23 00:52:47 +0530

    Time: 85 ms (70.20%), Space: 57.4 MB (65.10%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,29 +1,41 @@
 class Solution {
 public:
+    //previous submission complexity was o(2^n(2*n))
+    //complexity of this code is O(2^N)
     bool isP(string str,int l,int r){
         if(l>=r) return true;
         if(str[l]!=str[r])
             return false;
         return isP(str,l+1,r-1);
     }
-    void solve(int ind,string s,vector<string> &path,vector<vector<string>> &ans){
+    void solve(int ind,string s,vector<string> &path,vector<vector<string>> &ans,vector<vector<bool>>&dp){
         if(ind>=s.size()){
             ans.push_back(path);
             return;
         }
-    
+        string str="";
         for(int i=ind;i<s.size();i++){
-            if(isP(s,ind,i)){
-                path.push_back(s.substr(ind,i-ind+1));
-                solve(i+1,s,path,ans);
+            str+=s[i];
+            if(dp[ind][i]){
+                path.push_back(str);
+                solve(i+1,s,path,ans,dp);
                 path.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
+        int n=s.size();
+        vector<vector<bool>> dp(n,vector<bool>(n,0));
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(isP(s,i,j)){
+                    dp[i][j]=1;
+                }
+            }
+        }
         vector<vector<string>> ans;
         vector<string> path;
-        solve(0,s,path,ans);
+        solve(0,s,path,ans,dp);
         return ans;
     }
 };
