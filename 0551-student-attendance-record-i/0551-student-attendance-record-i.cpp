3a309beea82077865af731283647cc42ddf0150c
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-05-26 13:49:48 +0530

    Time: 4 ms (18.68%), Space: 7.6 MB (11.09%) - LeetHub

---
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkRecord(string s) {
+        int n=s.size();
+        vector<int> dp(n,0);
+        dp[0]=(s[0]=='L');
+        for(int i=1;i<n;i++){
+            if(s[i]=='L'){
+                dp[i]=dp[i-1]+1;
+            }
+            else{
+                dp[i]=0;
+            }
+        }
+        for(auto it:dp) cout<<it<<" ";
+        int cnt=1;
+        for(int i=0;i<n;i++){
+            if(s[i]=='A') cnt--;
+            if(cnt<0||dp[i]>=3) return false;
+            
+        }
+        return true;
+    }
+};
