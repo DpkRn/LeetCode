76f77c14587159e43f1f1e12946eae5ee0fc7756
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-06-01 12:27:11 +0530

    Time: 2311 ms (45.94%), Space: 0B (100.00%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,2 +1,3 @@
 # Write your MySQL query statement below
-select product_name, year, price from Product a right outer join Sales b on a.product_id=b.product_id;
+# select product_name, year, price from Product a right outer join Sales b on a.product_id=b.product_id;
+select product_name, year, price from Sales a left  join Product b on a.product_id=b.product_id;
