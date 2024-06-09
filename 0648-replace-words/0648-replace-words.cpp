0a454b5e92cb0b30c9f71b0a188075a90dec3f6c
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-06-10 02:53:12 +0530

    Time: 70 ms (82.81%), Space: 78.3 MB (45.31%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -1,3 +1,63 @@
+class TrieNode{
+    public:
+    bool isCompleted;
+    string word;
+    TrieNode* children[26];
+    TrieNode(){
+        word="";
+        memset(children,0,sizeof(children));
+        isCompleted=false;
+    }
+};
+class Solution {
+    
+public:
+    void insert(TrieNode* root,string word){
+        TrieNode* node=root;
+        for(auto ch:word){
+            int idx=ch-97;
+            if(node->children[idx]==NULL) node->children[idx]=new TrieNode();
+            node=node->children[idx];
+        }
+        node->word=word;
+        node->isCompleted=true;
+    }
+    string search(TrieNode* root,string word){
+        TrieNode* node=root;
+        for(auto ch:word){
+            int idx=ch-97;
+            if(node->children[idx]==NULL) return "";
+            node=node->children[idx];
+            if(node->isCompleted) return node->word;
+        }
+        return node->word;
+    }
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        TrieNode* root=new TrieNode();
+        for(auto word:dictionary){
+            insert(root,word);
+        }
+        stringstream ss(sentence);
+        vector<string> st;
+        string temp;
+        while(ss>>temp){
+            st.push_back(temp);
+        }
+        string ans="";
+        for(auto str:st){
+            string der=search(root,str);
+            if(der=="") ans+=str;
+            else ans+=der;
+            ans+=" ";
+        }
+        ans.pop_back();
+        return ans;
+        
+    }
+};
+/*
+//using hash brute force 
+
 class Solution {
 public:
     string replaceWords(vector<string>& dictionary, string sentence) {
@@ -35,3 +95,4 @@ public:
         return ans;
     }
 };
+*/
