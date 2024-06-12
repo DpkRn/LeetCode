ac56d6cf2c7cbf35edf823bf5cd6832a993eb4d8
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-06-13 02:05:24 +0530

    Time: 120 ms (66.54%), Space: 30.5 MB (83.37%) - LeetHub

--- 3039-apply-operations-to-make-string-empty/3039-apply-operations-to-make-string-empty.cpp
+++ 3039-apply-operations-to-make-string-empty/3039-apply-operations-to-make-string-empty.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
     string lastNonEmptyString(string s) {
+         //cnt all the maximum freq and make the string of character of maximum frequency in given order;
+        vector<int> freq(26,0);
+        int maxi=-1e9;
+        for(auto c:s) {
+            freq[c-'a']++;
+            maxi=max(maxi,freq[c-'a']);
+        }
+        if(maxi==1) return s;
+        string str="";
+        for(int i=s.size()-1;i>=0;i--){
+            if(freq[s[i]-'a']==maxi) {
+                str+=s[i];
+                freq[s[i]-'a']=-1; //set as minimum so character in ans not repeated
+            }
+        }
+        reverse(str.begin(),str.end());
+        return str;
+    }
+};
+/*
+class Solution {
+public:
+    string lastNonEmptyString(string s) {
+        //cnt all the maximum freq and make the string of character of maximum frequency in given order;
         vector<int> freq(26,0);
         for(auto c:s) freq[c-97]++;
         priority_queue<pair<int,char>> pq;
@@ -34,3 +58,4 @@ public:
     }
     
 };
+*/
