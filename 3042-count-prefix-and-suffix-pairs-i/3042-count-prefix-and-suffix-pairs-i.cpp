fa1ecd868cc37a938d5ee26dca79b111ae5bce82
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-06-12 21:18:08 +0530

    Time: 19 ms (15.48%), Space: 23.3 MB (50.62%) - LeetHub

--- 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.cpp
+++ 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     bool isPrefixAndSuffix(string str1, string str2){
+        int i=0,j=str1.size()-1;
+        int l=0,r=str2.size()-1;
         if(str1.size()>str2.size()) return false;
-        int n=str1.size();
-        string fromLeft=str2.substr(0,n);
-        string fromRight=str2.substr(str2.size()-n,n);
-        if(str1==fromLeft&&str1==fromRight) return true; return false;
+        while(i<str1.size()){
+            if(str1[i]!=str2[l]||str1[j]!=str2[r]) return false;
+            i++;j--;l++;r--;
+        }
+        return true;
     }
+    // bool isPrefixAndSuffix(string str1, string str2){
+    //     if(str1.size()>str2.size()) return false;
+    //     int n=str1.size();
+    //     string fromLeft=str2.substr(0,n);
+    //     string fromRight=str2.substr(str2.size()-n,n);
+    //     if(str1==fromLeft&&str1==fromRight) return true; return false;
+    // }
     int countPrefixSuffixPairs(vector<string>& words) {
         int cnt=0,n=words.size();
         
