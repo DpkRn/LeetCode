5b4aad0d21dfd7a49b62a1def659709abc43abf3
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-07-08 00:27:59 +0530

    Time: 3 ms (13.78%), Space: 7.1 MB (30.43%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int cnt=0;
+        while(numBottles>=numExchange){
+            numBottles=numBottles-numExchange+1;
+            cnt+=numExchange;
+        }
+        cnt+=numBottles;
+        return cnt;
+    }
+};
