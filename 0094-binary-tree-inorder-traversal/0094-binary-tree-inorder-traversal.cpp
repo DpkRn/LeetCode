c1872197e96006f838990fc98b32c7ad487cf40d
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-07-07 11:25:33 +0530

    Time: 4 ms (31.72%), Space: 9.4 MB (99.32%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -9,6 +9,31 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ 
+class Solution {
+public:
+//moris traversal O(N) O(1)
+    vector<int> inorderTraversal(TreeNode* root) {
+        TreeNode*  curr=root;
+        vector<int> inorder;
+        while(curr!=NULL){
+            if(curr->left==NULL){
+                inorder.push_back(curr->val);
+                curr=curr->right;
+            }else{
+                TreeNode* ryt=curr->left;
+                while(ryt->right!=NULL) ryt=ryt->right;
+                ryt->right=curr;
+                TreeNode* temp=curr;
+                curr=curr->left;
+                temp->left=NULL;
+            }
+        }
+        return inorder;
+    }
+};
+/*
+//using recursion O(N) O(N)
 class Solution {
     vector<int> ans;
 public:
@@ -23,3 +48,4 @@ public:
        return ans;
     }
 };
+*/
