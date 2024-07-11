d42818cf3b64777d3c300da49b57c9906af9445d
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-07-11 15:36:38 +0530

    Time: 3 ms (79.28%), Space: 10 MB (51.94%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,54 +1,42 @@
 class Solution {
-    bool isValidMove(int c,int r,vector<string> &chess,int n){
-        int x=c; int y=r;
-        //checking upper  diagonal
+public:
+    vector<vector<string>> ans;
+    vector<string> board;;
+    bool isValid(int r,int c){
+        int n=board.size();
+         int x=r,y=c;
+        for(int i=0;i<=r;i++){
+                if(board[i][c]=='Q') return false;            
+        }
+       
         while(x>=0&&y>=0){
-            if(chess[y][x]=='Q')
-                return false;
+            if(board[x][y]=='Q') return false;
             x--;y--;
         }
-        
-        x=c;y=r;
-         //checking left  
-        while(x>=0){
-            if(chess[y][x]=='Q')
-                return false;
-            x--;
-        }
-          //checking down  diagonal
-        x=c;y=r;
-        while(x>=0&&y<n){
-            if(chess[y][x]=='Q')
-                return false;
-            x--;y++;
+         while(r>=0&&c>=0){
+            if(board[r][c]=='Q') return false;
+            r--;c++;
         }
         return true;
-        
-        
     }
-    void solve(int c,vector<string> &chess,vector<vector<string>> &ans,int n){
-        if(c>=n){
-            ans.push_back(chess);
-            return;
+    void solve(int i){
+        if(i==board.size()){
+           ans.push_back(board);
+           return ;
         }
-        for(int r=0;r<n;r++){
-            if(isValidMove(c,r,chess,n)){
-                chess[r][c]='Q';
-                solve(c+1,chess,ans,n);
-                chess[r][c]='.';
+        for(int j=0;j<board.size();j++){
+            if(isValid(i,j)==1){
+                board[i][j]='Q';
+                solve(i+1);
+                board[i][j]='.';
             }
         }
     }
-public:
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ans;
-        vector<string>chess;
-        string str(n,'.');
-        for(int i=0;i<n;i++){
-            chess.push_back(str);
-        }
-        solve(0,chess,ans,n);
+        string str="";
+        for(int i=0;i<n;i++) str.push_back('.');
+        board=vector<string>(n,str);
+        solve(0);
         return ans;
-        
     }
 };
