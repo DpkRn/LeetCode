1f7855b71f6b44f6b0d44e82d63f161aea370ddf
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-07-12 19:15:22 +0530

    Time: 74 ms (72.36%), Space: 26 MB (38.62%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
-int get(int x, int y, string s1,string s2,string s){
+int get(int x,string s1,string &s){
     int n=s.size();
     string str;
     int score=0;
     for(int i=0;i<n;i++){
-        if(s[i]==s2[1]&&!str.empty()&&str.back()==s2[0]){
-            str.pop_back();
-            score+=y;  
-        }else{
-            str.push_back(s[i]);
-        }
-    }
-    s=str;
-    str="";
-    n=s.size();
-     for(int i=0;i<n;i++){
         if(s[i]==s1[1]&&!str.empty()&&str.back()==s1[0]){
             str.pop_back();
             score+=x;  
         }else{
             str.push_back(s[i]);
         }
-    }
+    } 
+    s=str;
     return score;
 }
     int maximumGain(string s, int x, int y) {
-       
-       if(y>x) return get(x,y,"ab","ba",s);
-       else return get(y,x,"ba","ab",s);
+       string s1="ab",s2="ba";
+       if(x<y){
+        swap(x,y); swap(s1,s2);
+       }
+       return get(x,s1,s)+get(y,s2,s);
     }
 };
