53049c69d7a7380b4b4cc9dafb8ff617076f0e95
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-07-21 01:29:41 +0530

    Time: 111 ms (25%), Space: 111.2 MB (75%) - LeetHub

--- 3498-minimum-array-changes-to-make-differences-equal/3498-minimum-array-changes-to-make-differences-equal.cpp
+++ 3498-minimum-array-changes-to-make-differences-equal/3498-minimum-array-changes-to-make-differences-equal.cpp
@@ -1,61 +1,44 @@
-int find(vector<int> &arr, int val)
-{
-    int low = 0;
-    int high = arr.size()-1;
-    
-    int mid;
-    int ans = -1;
-
-    while(low <= high)
-    {
-        int mid = (low+high)/2;
-        int curr = arr[mid];
-        
-        if(curr < val)
-        {
-            low = mid+1;
-            ans = mid;
-        }
-        
-        else if(curr >= val)
-            high = mid-1;
-    }
-    
-    return ans+1;
-}
-
 class Solution {
 public:
     int minChanges(vector<int>& nums, int k) {
-        
-        int n = nums.size();
+        int n=nums.size();
+        vector<int> left,right;
 
-        unordered_map<int, int> m;
-        vector<int> v;
-        
-        for(int i=0; i<n/2; ++i)
-        {
-            m[ abs( nums[i] - nums[n-i-1] ) ]++;
-            
-            int mx1 = max(k-nums[i], nums[i]); // max for num[i].
-            int mx2 = max(k - nums[n-i-1], nums[n-i-1]); // max for num[n-i-1]
-            
-            int mx = max(mx1, mx2); // Take the maximum among the two
-            v.push_back(mx);
+        for(int i=0;i<n/2;i++){
+            left.push_back(nums[i]);
+            right.push_back(nums[n-i-1]);
+             if(left[i]>right[i]) swap(left[i],right[i]);
         }
-        
-        sort(v.begin(), v.end());  // sorting required for binary search.
-        int ans = 1e9;
-        
-        for(auto i=m.begin(); i!=m.end(); ++i)
-        {
-            int val = i->first;
-            int fr = i->second;
-                
-            int a = find(v, val); // binary search
-            ans = min(n/2 - fr + a, ans); // keep track of the lowest operation count.
+
+       
+         vector<int> dp(k+1,0);
+         for(int i=0;i<n/2;i++){
+            int range=right[i]-left[i];
+            dp[range]++;
+         }
+
+        priority_queue<pair<int,int>>pq;
+
+         for(int i=0;i<k+1;i++){
+            if(dp[i]>0){
+                pq.push({dp[i],i});
+            }
+         }
+        int cnt=1e9;;
+        while(!pq.empty()){
+            int xy=pq.top().second;
+            cout<<xy<<" ";
+            pq.pop();
+            int t=0;
+            for(int i=0;i<n/2;i++){
+                if(right[i]-left[i]==xy) continue;
+                t++;
+                if(right[i]-xy<0&&left[i]+xy>k) t++;
+            }
+           if(cnt<t) return cnt;
+            cnt=min(cnt,t);
+            cout<<t<<endl;
         }
-        
-        return ans;
+        return cnt;
     }
 };
