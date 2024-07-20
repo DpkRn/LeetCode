418ac65b2c2a0c85bddb4ac49194b5604744cbb8
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-07-21 02:00:52 +0530

    Time: 169 ms (25%), Space: 108.5 MB (75%) - LeetHub

--- 3498-minimum-array-changes-to-make-differences-equal/3498-minimum-array-changes-to-make-differences-equal.cpp
+++ 3498-minimum-array-changes-to-make-differences-equal/3498-minimum-array-changes-to-make-differences-equal.cpp
@@ -2,6 +2,31 @@ class Solution {
 public:
     int minChanges(vector<int>& nums, int k) {
         int n=nums.size();
+        unordered_map<int,int> freq;
+        vector<int> v;
+        for(int i=0;i<n/2;i++){
+            int a=nums[i],b=nums[n-i-1];
+            freq[abs(b-a)]++;
+            int maxChange=max(max(a,b),k-min(a,b));//either minm->0 then x<=max or max->k i.0 x<=k-min(a,b)
+            // then only 1+ otherwize we need to decr minm and increase max to get in range i.e 2+
+            v.push_back(maxChange);
+        }
+        sort(v.begin(),v.end());
+        int ans=1e9;
+        for(auto it:freq){
+            int x=it.first; int cnt=it.second;
+            int ind=lower_bound(v.begin(),v.end(),x)-v.begin();
+            ans=min(ans,n/2-cnt+(ind));
+        }
+        return ans;
+    }
+};
+
+/*
+class Solution {
+public:
+    int minChanges(vector<int>& nums, int k) {
+        int n=nums.size();
         vector<int> left,right;
 
         for(int i=0;i<n/2;i++){
@@ -10,7 +35,6 @@ public:
              if(left[i]>right[i]) swap(left[i],right[i]);
         }
 
-       
          vector<int> dp(k+1,0);
          for(int i=0;i<n/2;i++){
             int range=right[i]-left[i];
@@ -42,3 +66,5 @@ public:
         return cnt;
     }
 };
+
+*/
