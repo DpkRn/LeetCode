1ed722f2c2f0dcbf5829ea5961cba6ff139526a4
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-07-24 16:19:46 +0530

    Time: 0 ms (100%), Space: 7.9 MB (43.75%) - LeetHub

--- 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -2,6 +2,36 @@ class Solution {
 public:
     string reverseParentheses(string s) {
         stack<int>st;
+        int n=s.size();
+        vector<int> ind(n,-1);
+        
+        for(int i=0;i<n;i++){
+            if(s[i]=='(') st.push(i);
+            else if(s[i]==')'){
+                ind[st.top()]=i;
+                ind[i]=st.top();
+                st.pop();
+            }
+        }
+        string str="";
+        int dir=1;
+        for(int i=0;i<n;i+=dir){
+            if(s[i]=='(' || s[i]==')'){
+                    dir*=-1;
+                    i=ind[i];
+            }else str+=s[i];
+        }
+
+        return str;
+    }
+};
+
+
+/*
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        stack<int>st;
         string rev;
         for(int i=0;i<s.size();i++){
             if(s[i]=='(')st.push(i);
@@ -20,3 +50,4 @@ public:
         return rev;
     }
 };
+*/
