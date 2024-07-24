a42d2e3741ca79ff284fd65131638c36443d649c
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-07-24 15:38:08 +0530

    Time: 18 ms (94.95%), Space: 22.8 MB (92.66%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.cpp
+++ 2502-sort-the-people/2502-sort-the-people.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+    int n=heights.size();
+    vector<int> sorted(n);
+    vector<string> ans(n);
+    for(int i=0;i<n;i++) sorted[i]=i;
+    sort(sorted.begin(),sorted.end(),[&](int a,int b){
+        return heights[a]>heights[b];
+    });
+
+    for(int i=0;i<n;i++){
+        ans[i]=names[sorted[i]];
+    }
+    return ans;
+}
+};
+/*
+
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
     unordered_map<int,int> mp;
     int i=0;
     for(auto it:heights) mp[it]=i++;
@@ -10,3 +29,4 @@ public:
     return ans;
 }
 };
+*/
