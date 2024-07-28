665b866c0ab3b3f3e9a3843058eb1b846444ad11
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-07-28 13:51:09 +0530

    Time: 40 ms (20.67%), Space: 16.1 MB (27.74%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,47 +1,44 @@
 class Solution {
 public:
-    void findDist(int src,vector<vector<int>> adj[],vector<vector<int>> &dist){
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        pq.push({0,src});
-        while(!pq.empty()){
-            int u=pq.top().second;
-            int d=pq.top().first;
-            pq.pop();
-            for(auto it:adj[u]){
-                if(d+it[1]<dist[src][it[0]]){
-                    dist[src][it[0]]=d+it[1];
-                    pq.push({dist[src][it[0]],it[0]});
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>> dp(n,vector<int>(n,1e9));
+        vector<int> mp(n);
+        for(auto it:edges){
+            dp[it[0]][it[1]]=it[2];
+            dp[it[1]][it[0]]=it[2];
+        }
+       
+       
+         for(int i=0;i<n;i++){
+            dp[i][i]=0;
+            mp[i]=i;
+          
+        }
+        vector<int> cnt(n,0);
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(dp[i][k]!=1e9&&dp[k][j]!=1e9&&(dp[i][j]>dp[i][k]+dp[k][j])){
+                    dp[i][j]=dp[i][k]+dp[k][j];
+                    }
                 }
             }
         }
-    }
-    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>> adj[n];
-        for(auto vec:edges){
-            adj[vec[0]].push_back({vec[1],vec[2]});
-            adj[vec[1]].push_back({vec[0],vec[2]});
-        }
-        
-        vector<vector<int>>dist(n,vector<int> (n,1e8));
         for(int i=0;i<n;i++){
-                dist[i][i]=0;
+           for(int j=0;j<n;j++){
+            if(i!=j&&dp[i][j]<=distanceThreshold)
+            cnt[i]++;
+            cout<<dp[i][j]<<" ";
+           }
+           cout<<endl;
         }
-        for(int i=0;i<n;i++){
-            findDist(i,adj,dist);
-        }
-        vector<bool> cnt(n,0);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                cout<<dist[i][j]<<" ";
-            }
-            cout<<endl;
-        }
-        int ans=-1;
+          
+       int ans=-1;
        int mini=n;
         for(int i=0;i<n;i++){
              int city=0;
             for(int j=0;j<n;j++){
-                if(i!=j&&dist[i][j]<=distanceThreshold) city++;
+                if(i!=j&&dp[i][j]<=distanceThreshold) city++;
             }
            if(city<=mini){
                ans=i;
@@ -49,7 +46,5 @@ public:
            }
         }
         return ans;
-        
-        
     }
 };
