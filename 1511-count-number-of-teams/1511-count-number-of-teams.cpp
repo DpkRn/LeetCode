4228241fb639357ff4af579d05afef441c46e778
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-07-29 22:08:33 +0530

    Time: 39 ms (54.16%), Space: 11.6 MB (51.74%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.cpp
+++ 1511-count-number-of-teams/1511-count-number-of-teams.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-    int get(auto &rating){
-        int n=rating.size();
-        int res=0;
-        for(int i=0;i<n;i++){
-            int smaller=0;int greater=0;
-            for(int j=0;j<n;j++){
-                if(j<i&&rating[j]<rating[i]){
-                    smaller++;
-                }
-                if(i<j&&rating[j]>rating[i]){
-                    greater++;
-                }
-            }
-            res+=(smaller*greater);
-        }
-        return res;
-    }
     int numTeams(vector<int>& rating) {
        int n=rating.size();
-       int ans=get(rating);
-       reverse(rating.begin(),rating.end());
-       ans+=get(rating);
-       return ans;
+      int res=0;
+        for(int i=0;i<n;i++){
+            int less[2]={}; int greater[2]={};
+            for(int j=0;j<n;j++){
+                if(rating[j]<rating[i]){
+                    less[j<i]++;
+                }
+                if(rating[j]>rating[i]){
+                    greater[j<i]++;
+                }
+            }
+            res+=less[0]*greater[1]+less[1]*greater[0];
+        }
+        return res;
     }
 };
