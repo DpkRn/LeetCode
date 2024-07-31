93eba201af998e66e408d9d80a90934d0ce83c99
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-07-31 23:29:33 +0530

    Time: 7 ms (28.25%), Space: 12 MB (11.42%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,68 +1,45 @@
-//using Trie Concept its long but if want to show short method look below using greedy
 class TrieNode{
     public:
-    bool isCompleted;
-    TrieNode* children[26];
+    TrieNode *children[26];
+    bool isCom;
     TrieNode(){
-        memset(children,0,sizeof(children));
-        isCompleted=false;
-    }
-};
-class Solution {
-public:
-    string longestCommonPrefix(vector<string>& strs) {
-        TrieNode* root=new TrieNode();
-        
-        for(auto str:strs){
-            if(str=="") return "";
-            TrieNode* node=root;
-            for(auto ch:str){
-                int idx=ch-97;
-                if(node->children[idx]==NULL) node->children[idx]=new TrieNode();
-                node=node->children[idx];
-            }
-            
-            node->isCompleted=true;
-        }
-        
-        
-       TrieNode* node=root;
-        string ans="";
-        while(true){
-            int cnt=0,idx=0;
-            for(int i=0;i<26;i++){
-                if(node->children[i]!=NULL){
-                   cnt++;
-                   idx=i;
-                }
-            }
-            if(cnt!=1) return ans;
-            char ch=97+idx;
-            ans.push_back(ch);       
-            node=node->children[idx];
-            if(node->isCompleted==true) return ans;
-            
-        }
-        return ans;
+        memset(children,NULL,sizeof(children));
+        isCom=false;
     }
 };
 
-//using greedy
-/*
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& v) {
+  
+    string longestCommonPrefix(vector<string>& strs) {
+        TrieNode* root=new TrieNode();
         string ans="";
-        sort(v.begin(),v.end());
-        int n=v.size();
-        string first=v[0],last=v[n-1];
-        for(int i=0;i<min(first.size(),last.size());i++){
-            if(first[i]!=last[i]){
-                return ans;
-            }
-            ans+=first[i];
+        for(auto str:strs){
+             TrieNode* node=root;
+             for(char ch:str){
+
+                int idx=ch-'a';
+                if(node->children[idx]==NULL) node->children[idx]=new TrieNode();
+                node=node->children[idx];  
+             }
+             node->isCom=true;
+             
         }
-        return ans;
+        string temp=strs[0];
+        TrieNode* node=root;
+        for(auto ch:temp){
+            int cnt=0;
+            for(int i=0;i<26;i++){
+                if(node->children[i]!=NULL){
+                    cnt++;
+                }
+            }
+             if(cnt!=1 or node->isCom==true) return ans;
+                ans+=ch;
+            node=node->children[ch-'a'];
+        }
+       
+     return ans;
+
     }
 };
-*/
