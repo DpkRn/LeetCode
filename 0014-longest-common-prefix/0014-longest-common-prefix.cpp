f23d844eb2b37dd77712a803854587414012f721
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-07-31 23:49:55 +0530

    Time: 0 ms (100%), Space: 11.2 MB (35.82%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,45 +1,16 @@
-class TrieNode{
-    public:
-    TrieNode *children[26];
-    bool isCom;
-    TrieNode(){
-        memset(children,NULL,sizeof(children));
-        isCom=false;
-    }
-};
-
 class Solution {
 public:
-  
-    string longestCommonPrefix(vector<string>& strs) {
-        TrieNode* root=new TrieNode();
+    string longestCommonPrefix(vector<string>& v) {
         string ans="";
-        for(auto str:strs){
-             TrieNode* node=root;
-             for(char ch:str){
-
-                int idx=ch-'a';
-                if(node->children[idx]==NULL) node->children[idx]=new TrieNode();
-                node=node->children[idx];  
-             }
-             node->isCom=true;
-             
-        }
-        string temp=strs[0];
-        TrieNode* node=root;
-        for(auto ch:temp){
-            int cnt=0;
-            for(int i=0;i<26;i++){
-                if(node->children[i]!=NULL){
-                    cnt++;
-                }
+        sort(v.begin(),v.end());
+        int n=v.size();
+        string first=v[0],last=v[n-1];
+        for(int i=0;i<min(first.size(),last.size());i++){
+            if(first[i]!=last[i]){
+                return ans;
             }
-             if(cnt!=1 or node->isCom==true) return ans;
-                ans+=ch;
-            node=node->children[ch-'a'];
+            ans+=first[i];
         }
-       
-     return ans;
-
+        return ans;
     }
 };
