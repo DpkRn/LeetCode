591c3b2e2a11c2995479b25cfdc83ff56072d62f
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-08-01 11:12:52 +0530

    Time: 6 ms (60.14%), Space: 17.7 MB (16.71%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
@@ -3,9 +3,9 @@ public:
     int countSeniors(vector<string>& details) {
         int cnt=0;
         for(auto detail:details){
-            if(stoi(detail.substr(11,2))>60) {
+            if(detail.substr(11,2)>"60") {
                 cnt++;
-                cout<<detail.substr(11,2)<<endl;
+                //cout<<detail.substr(11,2)<<endl;
             }
         }
         return cnt;
