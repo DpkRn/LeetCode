2ab9c9426d4a63d2f233d54d5bb759d42b9ae12a
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-08-04 21:57:29 +0530

    Time: 421 ms (6.97%), Space: 11.5 MB (77.98%) - LeetHub

--- 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
@@ -1,19 +1,20 @@
+#define M 1000000007
+typedef pair<int,int> P;
 class Solution {
 public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
-        vector<int>  sums;
-        for(int i=0;i<n;i++){
-            int sum=0;
-            for(int j=i;j<n;j++){
-                sum+=nums[j];
-                sums.push_back(sum);
+        priority_queue<P,vector<P>,greater<P>> pq;
+        for(int i=0;i<n;i++) pq.push({nums[i],i});
+        long long result=0;
+        for(int cnt=1;cnt<=right;cnt++){
+            auto [sum,idx]=pq.top();
+            pq.pop();
+            if(cnt>=left){
+               cout<<sum<<" "<<idx<<endl;
+               result=(result+sum)%M;
             }
+            if(idx+1<n) pq.push({sum+nums[idx+1],idx+1});
         }
-        sort(sums.begin(),sums.end());
-        long long sum=0;
-        for(int i=left-1;i<=right-1;i++){
-            sum=(sum+sums[i])%int(1e9+7);
-        }
-        return sum;
+        return result;
     }
 };
