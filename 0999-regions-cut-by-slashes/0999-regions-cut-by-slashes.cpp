068c7377070d4d7bf63c58e32dfec80988a3fdc2
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-08-11 03:11:00 +0530

    Time: 16 ms (23.24%), Space: 13.7 MB (26.89%) - LeetHub

--- 0999-regions-cut-by-slashes/0999-regions-cut-by-slashes.cpp
+++ 0999-regions-cut-by-slashes/0999-regions-cut-by-slashes.cpp
@@ -1,46 +1,24 @@
 class Solution {
 public:
-    int dx[4]={0,0,-1,1};
-    int dy[4]={1,-1,0,0};
-    void dfs(int r,int c,vector<vector<int>> &mat){
-        mat[r][c]=1;
-        for(int i=0;i<4;i++){
-            int x=dx[i]+r; int y=dy[i]+c;
-            if(x>=0&&x<mat.size()&&y>=0&&y<mat[0].size()&&mat[x][y]==0){
-                dfs(x,y,mat);
-            }
-        }
-
+    int dfs(vector<vector<int>>& g, int i, int j) {
+        if (min(i,j)<0||max(i,j)>=g.size()||g[i][j]!=0) return 0;
+        g[i][j]=1;
+        return 1+dfs(g,i-1,j)+dfs(g,i+1,j)+dfs(g,i,j-1)+dfs(g,i,j+1);
     }
-    int regionsBySlashes(vector<string>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        vector<vector<int>> mat(m*3,vector<int>(n*3,0));
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]=='\\'){
-                    mat[i*3][j*3]=1; mat[i*3+1][j*3+1]=1; mat[i*3+2][j*3+2]=1;
-                }else if(grid[i][j]=='/'){
-                    mat[i*3][j*3+2]=1; mat[i*3+1][j*3+1]=1; mat[i*3+2][j*3]=1;
-                }
-            }
-        }
 
-        // for(int i=0;i<m*3;i++){
-        //     for(int j=0;j<n*3;j++){
-        //         cout<<mat[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        int cnt=0;
-        for(int i=0;i<m*3;i++){
-            for(int j=0;j<n*3;j++){
-                if(mat[i][j]==0){
-                    dfs(i,j,mat);
-                    cnt++;
-                }
-            }
-        }
-        return cnt;
+    int regionsBySlashes(vector<string>& grid) {
+        int n = grid.size(), regions = 0;
+        vector<vector<int>> g(n * 3, vector<int>(n * 3, 0));
+        for (int i=0;i<n;++i)
+            for (int j=0;j<n;++j)
+                if (grid[i][j]=='/')
+                    g[i*3][j*3+2]=g[i*3+1][j*3+1]=g[i*3+2][j*3]=1;
+                else if (grid[i][j]=='\\')
+                    g[i*3][j*3]=g[i*3+1][j*3+1] =
+                        g[i*3+2][j*3+2]=1;
+        for (int i=0;i<n*3;++i)
+            for (int j=0;j<n*3;++j)
+                regions += dfs(g, i, j) ? 1 : 0;
+        return regions;
     }
 };
