66531a9bb3890024e4a013b13f9fac6767f96bc9
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-08-19 09:14:02 +0530

    Time: 0 ms (100%), Space: 7.5 MB (71.95%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,30 +1,14 @@
 class Solution {
  public:
-//     int dfs(int i,int n,int l,auto &dp){
-//         if(i==n){
-//             return 0;
-//         }
-//         if(dp[i][l]!=-1) return dp[i][l];
-//         if(i>n) return 1e9;
-//         int left=2+dfs(2*i,n,i,dp);
-//         int right=1+dfs(i+l,n,l,dp);
-//         return dp[i][l]=min(left,right);
-//     }
-    int minSteps(int n) {
-        if(n==1) return 0; 
-        // vector<vector<int>> dp(2*n+1,vector<int>(n+1,-1));
-        // return dfs(1,n,1,dp)+1;
 
-        vector<vector<int>> dp(n+1,vector<int>(n+1,1e9));
-        for(int i=0;i<n;i++)
-        dp[n][i]=0;
-            for(int i=n-1;i>=1;i--){
-                for(int l=n;l>=1;l--){
-                    dp[i][l]=(2*i>n)?1e9:2+dp[2*i][i];
-                    dp[i][l]=(i+l>n)?1e9:min(dp[i][l],1+dp[i+l][l]);
-                }
+    int minSteps(int n) {
+        int s = 0;
+        for (int d = 2; d <= n; d++) {
+            while (n % d == 0) {
+                s += d;
+                n /= d;
             }
-            return 1+dp[1][1];
-        
+        }
+        return s;
     }
 };
