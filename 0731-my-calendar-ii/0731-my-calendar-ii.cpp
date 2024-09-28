3ecd044e0310518c0c03998a50bffd18eefdbf98
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-09-28 10:29:25 +0530

    Time: 172 ms (11.44%), Space: 42.6 MB (70.23%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.cpp
+++ 0731-my-calendar-ii/0731-my-calendar-ii.cpp
@@ -1,42 +1,28 @@
 class MyCalendarTwo {
+    map<int,int> mp;
 public:
-    // Store the number of bookings at each point.
-    map<int, int> bookingCount;
-    // The maximum number of overlapped bookings allowed.
-    int maxOverlappedBooking;
-
-    MyCalendarTwo() { maxOverlappedBooking = 2; }
-
+    MyCalendarTwo() {
+        
+    }
+    
     bool book(int start, int end) {
-        // Increase and decrease the booking count at the start and end
-        // respectively.
-        bookingCount[start]++;
-        bookingCount[end]--;
-
-        int overlappedBooking = 0;
-        // Find the prefix sum.
-        for (pair<int, int> bookings : bookingCount) {
-            overlappedBooking += bookings.second;
-
-            // If the number of bookings is more than 2, return false.
-            // Also roll back the counts for this booking as we won't add it.
-            if (overlappedBooking > maxOverlappedBooking) {
-                bookingCount[start]--;
-                bookingCount[end]++;
-
-                // Remove the entries from the map to avoid unnecessary
-                // iteration.
-                if (bookingCount[start] == 0) {
-                    bookingCount.erase(start);
-                }
-                if (bookingCount[end] == 0) {
-                    bookingCount.erase(end);
-                }
-
+        mp[start]++;
+        mp[end]--;
+        int events=0;
+        for(auto it:mp){
+            events+=it.second;
+            if(events==3){
+                mp[start]--;
+                mp[end]++;
                 return false;
             }
         }
-
         return true;
     }
 };
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(start,end);
+ */
