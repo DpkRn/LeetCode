8485a1d204f9d162486eb35fc7d68053985fe0e3
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-10-02 01:23:29 +0530

    Time: 134 ms (79.05%), Space: 68.9 MB (71.54%) - LeetHub

--- 2301-count-array-pairs-divisible-by-k/2301-count-array-pairs-divisible-by-k.cpp
+++ 2301-count-array-pairs-divisible-by-k/2301-count-array-pairs-divisible-by-k.cpp
@@ -1,18 +1,27 @@
-typedef long long ll;
-
 class Solution {
 public:
     long long countPairs(vector<int>& nums, int k) {
-        unordered_map<ll, ll> gcdCount;
-        ll ans = 0;
-        for (ll i = 0; i < nums.size(); ++i)
-        {
-            ll currgcd = __gcd(nums[i], k);
-            for (auto &[gc_d, count] : gcdCount)
-                if ((currgcd * gc_d) % k == 0)
-                    ans += count;
-            gcdCount[currgcd]++;
+         unordered_map<int,int> factorCount;
+        long long result =0;
+        int previousIndexCount = 0; // stores any Index that was traversed before
+        for (int i=0;i<nums.size(); i++){
+            int factor = gcd(nums[i], k); // this is the "useful" part of nums[i]
+            int missingFactor = k/factor; // what other factor is needed to form k
+            
+            if (missingFactor==1){ // if missingFactor is 1, the that means nums[i] can match with any previous index to form a pair, k*n %k == 0
+                result += previousIndexCount;
+            }else{ // if m is not 1, we can check if any previous index has the missing factor
+                for (auto it=factorCount.begin(); it!=factorCount.end(); it++){
+                    if ( it->first % missingFactor == 0){
+                        result += it->second;
+                    }
+                }
+            }
+            // only add the "useful" factor part to the map
+            factorCount[factor]++;
+            previousIndexCount++;
+            
         }
-        return ans;
+        return result;
     }
 };
