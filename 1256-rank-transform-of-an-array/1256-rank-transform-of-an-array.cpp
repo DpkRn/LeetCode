5b2dddcf4494ba21a2f9d35e5e372b6262522a25
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-10-03 00:58:53 +0530

    Time: 91 ms (39.77%), Space: 34.8 MB (99.51%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
+        int n=arr.size();
+        if(n==0) return {};
+       vector<int> ord(n);
+       for(int i=0;i<n;i++) ord[i]=i;
+       sort(ord.begin(),ord.end(),[&](int a,int b){
+          return arr[a]<arr[b];
+       });
+       for(auto it:ord) cout<<it<<" ";
+       vector<int> rank(n);
+       rank[ord[0]]=1;
+       int r=1;
+       for(int i=1;i<n;i++){
+        if(arr[ord[i-1]]==arr[ord[i]]) rank[ord[i]]=r;
+        else
+         rank[ord[i]]=++r;
+
+       }
+       return rank;
+
+    }
+};
+
+/*
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
      set<int> st(arr.begin(),arr.end());
 	unordered_map<int,int> m;
 	int rank=1;
@@ -9,3 +35,4 @@ public:
 	return arr;
     }
 };
+*/
