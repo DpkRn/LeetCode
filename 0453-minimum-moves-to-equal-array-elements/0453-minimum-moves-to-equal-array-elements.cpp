d0db0be6080f4e56d6d348d73c98efa3ad4152b4
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-10-04 16:41:00 +0530

    Time: 24 ms (77.68%), Space: 30.9 MB (80.06%) - LeetHub

--- 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.cpp
+++ 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
+//solve this equation  sum + m * (n - 1) = x * n   and   x = minNum + m where m=required moves ,x=element after m moves, minNum=minimum no. and sum=total sum
     int minMoves(vector<int>& nums) {
-       int mini=*min_element(nums.begin(),nums.end());
-       int sum=accumulate(nums.begin(),nums.end(),0);
        int n=nums.size();
+       int mini=1e9+1,sum=0;
+       for(auto &it:nums){
+        mini=min(mini,it); sum+=it;
+       }
        return sum-mini*n;
     }
 };
