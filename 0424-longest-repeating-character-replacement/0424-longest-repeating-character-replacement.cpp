e347e4e08eb09446c46017ddee4f379cc64c4de2
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-10-08 18:16:57 +0530

    Time: 12 ms (40.46%), Space: 9.5 MB (41.62%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,50 +1,17 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-            int n = s.size();
-            int maxLen = 0,l=0,maxi=0;          
-            int hash[26]={0};
-        for (int i = 0; i < n; i++){
-              hash[s[i]-'A']++;
-              maxi=max(maxi,hash[s[i]-'A']);
-              while((i-l+1-maxi)>k){
-                  hash[s[l]-'A']--;
-                  maxi=0;
-                  for(int j=0;j<26;j++){
-                      maxi=max(maxi,hash[j]);
-                  }
-                  l++;
-              }
-            if(i-l+1-maxi<=k)
-             maxLen=max(maxLen,i-l+1);   
+        int n=s.size();
+        if(n==0) return 0;
+        unordered_map<char,int> mp;
+        int l=0,maxCount=0;
+        for(int i=0;i<n;i++){
+            mp[s[i]]++;
+            maxCount=max(maxCount,mp[s[i]]);
+            if(((i-l+1)-maxCount)>k){
+                mp[s[l++]]--;
+            }
         }
-    return maxLen;
+        return n-l;
     }
 };
-/*
-class Solution
-{
-    public:
-        int characterReplacement(string s, int k)
-        {
-            int n = s.size();
-            int maxLen = 0;
-            for (int i = 0; i < n; i++)
-            {
-                int hash[26]={0};
-                int maxi = 0;
-                for (int j = i; j < n; j++)
-                {
-                    hash[s[j] - 'A']++;
-                    maxi = max(maxi, hash[s[j] - 'A']);
-                    int change = (j - i + 1 - maxi);
-                   // cout<<maxi<<" "<<change<<j-i+1<<endl;
-                    if (change <= k)
-                        maxLen = max(maxLen, j - i + 1);
-                    else break;
-                }
-            }
-            return maxLen;
-        }
-};
-*/
