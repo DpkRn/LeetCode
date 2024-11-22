17fd63c5c27e0ffea290fa389446242b3a14bc03
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-11-22 21:48:01 +0530

    Time: 0 ms (100%), Space: 25.9 MB (17.89%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -3,12 +3,9 @@ public:
     int trap(vector<int>& height) {
         int n=height.size();
         vector<int> next(n),prev(n);
-        cout<<next.size();  
-        for(int i=n-1;i>=0;i--){
-            next[i]= i==n-1?height[i]:max(height[i],next[i+1]);
-        }
         for(int i=0;i<n;i++){
             prev[i]= i==0?height[i]:max(height[i],prev[i-1]);
+            next[n-i-1]= n-i==n?height[n-1-i]:max(height[n-1-i],next[n-i]);
         }
         int ans=0;
         for(int i=0;i<n;i++){
