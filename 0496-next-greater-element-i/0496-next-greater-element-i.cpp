243622b80b1d65a86c27c966bd2bc95fbbebe1c7
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-11-22 15:55:25 +0530

    Time: 3 ms (50.07%), Space: 12.3 MB (45.46%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int n2=nums2.size();
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums1.size();i++){
+            mp[nums1[i]]=i;
+        }
+        int n=nums2.size();
+        vector<int> vec(nums1.size(),-1);
         stack<int> st;
-        vector<int> next(n2);
-        for(int i=n2-1;i>=0;i--){
-            
-            while(!st.empty()&&st.top()<=nums2[i]) st.pop();
-            if(!st.empty())
-                next[i]=st.top();
-            else 
-                next[i]=-1;
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty()&&(st.top()<=nums2[i])){
+                st.pop();
+            }
+            if(mp.find(nums2[i])!=mp.end())
+            vec[mp[nums2[i]]]=st.empty()?-1:st.top();
             st.push(nums2[i]);
         }
-        int n1=nums1.size();
-        vector<int> ans(n1);
-        for(int i=0;i<n1;i++){
-            for(int j=0;j<n2;j++){
-                if(nums1[i]==nums2[j]){
-                    ans[i]=next[j];
-                    break;
-                }
-            }
-        }
-        return ans;
+
+        return vec;
     }
 };
