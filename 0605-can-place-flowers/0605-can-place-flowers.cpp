4d9c0acce462c6c32b3f7d9056b53fd124cf0408
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-11-24 22:37:01 +0530

    Time: 0 ms (100%), Space: 23 MB (82.77%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -15,3 +15,29 @@ public:
         return cnt>=n;
     }
 };
+/*
+//for two gap
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int cnt=0;
+        for(int i=0;i<flowerbed.size();i++){
+            if(flowerbed[i]==0){
+                bool lleft=(i>1?flowerbed[i-2]==0:true);
+                bool left=(i>0?flowerbed[i-1]==0:true);
+                bool right=(i<flowerbed.size()-1?flowerbed[i+1]==0:true);
+                bool rright=(i<flowerbed.size()-2)?flowerbed[i+2]==0:true;
+                 
+                if(left&&right&&lleft&&rright){
+                    flowerbed[i]=1;
+                    cnt++;
+                    
+                }
+            }
+        }
+        for(auto it:flowerbed) cout<<it<<" ";
+        // cout<<cnt<<endl;
+        return cnt>=n;
+    }
+};
+*/
