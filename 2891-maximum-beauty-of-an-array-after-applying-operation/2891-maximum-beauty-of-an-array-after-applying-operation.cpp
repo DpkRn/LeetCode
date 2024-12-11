a5fcb31e9d3aa305eaa7df1ca20887c65aaa389c
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-12-11 22:02:44 +0530

    Time: 54 ms (55.89%), Space: 110.6 MB (48.58%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     int maximumBeauty(vector<int>& nums, int k) {
+
+        //suppose two no is (x,y) and i want to make equal by increasing and decreasing to k lets x<y then 
+        //it would be equal only its interval (x-k,x+k) overlaps with (y-k,y+k)
+        //condition to overlap (x+k)>=(y-k)
+        //y<=x+2*k
+        //and apply the merge interval below technique
+
+        int n=nums.size();
+        sort(begin(nums),end(nums));
+        deque<int> dq;
+        int ans=0;
+        for(auto it:nums){
+            while(!dq.empty()&&!(it<=dq.front()+2*k)) dq.pop_front();
+            dq.push_back(it);
+            ans=max(ans,(int)dq.size());
+
+        }
+        return ans;        
+    }
+};
+
+/*
+class Solution {
+public:
+//using interval
+    int maximumBeauty(vector<int>& nums, int k) {
         int n=nums.size();
         deque<pair<int,int>> deq;
         vector<pair<int,int>> intervals;
@@ -20,3 +46,4 @@ public:
         return ans;
     }
 };
+*/
