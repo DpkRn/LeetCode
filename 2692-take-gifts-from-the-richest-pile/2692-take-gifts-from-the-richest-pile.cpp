97b337a6b39a7c2fd69636ae83a1e87ef33f4612
Deepak Aryan <43015835+DpkRn@users.noreply.github.com>
2024-12-12 14:06:00 +0530

    Time: 0 ms (100%), Space: 12.9 MB (89.48%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
@@ -2,21 +2,26 @@ class Solution {
 public:
     long long pickGifts(vector<int>& gifts, int k) {
         priority_queue<int> pq;
+         long long ans=0;
         for(auto it:gifts){
             pq.push(it);
+            ans+=it;
         }
 
-        long long ans=0;
+       
         while(!pq.empty()&&k>0){
-            int t=sqrt(pq.top());
+            int top=pq.top();
+            ans-=top;
+            int t=sqrt(top);
             pq.pop();
             pq.push(t);
+            ans+=t;
             k--;
         }
-        while(!pq.empty()){
-            ans+=pq.top();
-            pq.pop();
-        }
+        // while(!pq.empty()){
+        //     ans+=pq.top();
+        //     pq.pop();
+        // }
         return ans;
 
     }
